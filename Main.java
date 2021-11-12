f86b49d840276da27688c72ec01213e604fb1812
OmariPratt <85505039+OmariPratt@users.noreply.github.com>
2021-11-12 02:42:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+class Main {
+  public static void main(String[] args) {
+    
+
+    String name = "Omari Pratt";
+    String timeManagement = "prioritizing assignments";
+    String Personalcommitment = "i want to manage my ability to focus "; 
+    String KPIMetrics = "needs more progress and needs to be ready for use ";
+
+    System.out.println(
+      "hello my name is " + name 
+      + ", the time management strategy that i would like to be held accountable to is " + timeManagement + ". \n i will personally commit to " + Personalcommitment+ ".\n " + "my bisiness' main KPI is are " + KPIMetrics); 
+  }
+}
